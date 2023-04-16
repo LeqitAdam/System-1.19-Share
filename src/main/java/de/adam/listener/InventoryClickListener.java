82b3395ad3c8454d1546592eeb35ecac7a47c897
Adam <aeromcde@gmail.com>
2023-04-17 01:59:24 +0200

    update

--- src/main/java/de/adam/listener/InventoryClickListener.java
+++ src/main/java/de/adam/listener/InventoryClickListener.java
@@ -1,8 +1,19 @@
 package de.adam.listener;
 
 import com.google.common.collect.Lists;
+import com.plotsquared.core.PlotAPI;
+import com.plotsquared.core.PlotSquared;
+import com.plotsquared.core.events.PlotPlayerEvent;
+import com.plotsquared.core.location.BlockLoc;
+import com.plotsquared.core.location.PlotLoc;
 import com.plotsquared.core.player.PlotPlayer;
+import com.plotsquared.core.plot.Plot;
+import com.plotsquared.core.plot.PlotItemStack;
+import com.plotsquared.core.plot.PlotManager;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +24,7 @@ import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import javax.swing.plaf.synth.Region;
 import java.util.List;
 import java.util.UUID;
 
@@ -40,35 +52,56 @@ public class InventoryClickListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onMove(final InventoryDragEvent e) {
-        if (getNoClick().contains(e.getWhoClicked().getUniqueId())) {
-            e.setCancelled(true);
+    public void onMove(final InventoryDragEvent event) {
+        if (getNoClick().contains(event.getWhoClicked().getUniqueId())) {
+            event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onBuild(final PlayerInteractEvent e) {
-        Player p = e.getPlayer();
-        if(!PlotPlayer.from(p).getLocation().isPlotRoad()) {
-            if(PlotPlayer.from(p).getCurrentPlot() != null) {
-                if(!(PlotPlayer.from(p).getCurrentPlot().isOwner(p.getUniqueId()) || PlotPlayer.from(p).getCurrentPlot().isAdded(p.getUniqueId()))) {
-                    checkPerms(p, e);
+    public void onInteract(final PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if(!PlotPlayer.from(player).getLocation().isPlotRoad()) {
+            if(player.hasPermission("plots.admin.interact.other")) {
+                if(!(PlotPlayer.from(player).getCurrentPlot().isOwner(player.getUniqueId()) || PlotPlayer.from(player).getCurrentPlot().isAdded(player.getUniqueId()))) {
+                    checkPerms(player, event);
                 }
+                /*PlotPlayer plotPlayer = PlotPlayer.from(player);
+                PlotAPI plotAPI = new PlotAPI();
+                Block block = event.getClickedBlock();
+                PlotLoc plotLoc = new PlotLoc(block.getX(), block.getY(), block.getZ());
+                BlockLoc blockLoc = new BlockLoc(block.getX(), block.getY(), block.getZ());
+                if(PlotPlayer.from(player).getCurrentPlot().getPosition().equals(blockLoc)) {
+                    player.sendMessage("Block erkannt");
+                }
+                if(PlotPlayer.from(player).getLocation().equals(plotLoc)) {
+                    player.sendMessage("Block erkannt 2");
+                }*/
             }
-        }else checkPerms(p, e);
+        }else {
+            if(player.hasPermission("plots.admin.interact.other")) {
+                checkPerms(player, event);
+            }
+        }
+    }
+    public Location convertToPlotSquaredLocation(org.bukkit.Location bukkitLocation) {
+        World worldName = bukkitLocation.getWorld();
+        int x = bukkitLocation.getBlockX();
+        int y = bukkitLocation.getBlockY();
+        int z = bukkitLocation.getBlockZ();
+        return new Location(worldName, x, y, z);
     }
     @EventHandler
-    public void onEntity(PlayerInteractEntityEvent e) {
-        Player p = e.getPlayer();
-        if(!PlotPlayer.from(p).getLocation().isPlotRoad()) {
-            if(PlotPlayer.from(p).getCurrentPlot() != null) {
-                if(!(PlotPlayer.from(p).getCurrentPlot().isOwner(p.getUniqueId())  || PlotPlayer.from(p).getCurrentPlot().isAdded(p.getUniqueId()))) {
-                    checkOther(p, e);
+    public void onEntity(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        if(!PlotPlayer.from(player).getLocation().isPlotRoad()) {
+            if(PlotPlayer.from(player).getCurrentPlot() != null) {
+                if(!(PlotPlayer.from(player).getCurrentPlot().isOwner(player.getUniqueId())  || PlotPlayer.from(player).getCurrentPlot().isAdded(player.getUniqueId()))) {
+                    checkOther(player, event);
                 }
             }
-        }else checkOther(p, e);
+        }else checkOther(player, event);
     }
-
     @EventHandler (priority = EventPriority.HIGHEST)
     public void onClose(final InventoryCloseEvent e) {
         if (getNoClick().contains(e.getPlayer().getUniqueId())) {
