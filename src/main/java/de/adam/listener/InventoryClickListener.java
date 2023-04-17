9c9fc510549d36ae47e3d97ae33802f997453922
Adam <aeromcde@gmail.com>
2023-04-17 02:13:24 +0200

    update

--- src/main/java/de/adam/listener/InventoryClickListener.java
+++ src/main/java/de/adam/listener/InventoryClickListener.java
@@ -1,5 +1,4 @@
 package de.adam.listener;
-
 import com.google.common.collect.Lists;
 import com.plotsquared.core.player.PlotPlayer;
 import org.bukkit.Material;
@@ -12,211 +11,207 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-
 import java.util.List;
 import java.util.UUID;
-
 public class InventoryClickListener implements Listener {
-
     public static List<UUID> getNoClick() {
         return noClick;
     }
     public static List<UUID> noClick = Lists.newArrayList();
-
+    private String adminperms = "plots.admin.interact.other";
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onclick(final InventoryClickEvent e) {
-
-        if(e.getCurrentItem() != null) {
-            if (getNoClick().contains(e.getWhoClicked().getUniqueId())) {
-                if(!e.getClickedInventory().equals(e.getWhoClicked().getInventory())) {
-                    e.setCancelled(true);
-                }else if(e.getClick().isShiftClick()) {
-                    e.setCancelled(true);
-                }else if(e.getClick() == ClickType.DOUBLE_CLICK) {
-                    e.setCancelled(true);
+    public void onclick(final InventoryClickEvent event) {
+        if(event.getCurrentItem() != null) {
+            if (getNoClick().contains(event.getWhoClicked().getUniqueId())) {
+                if(!event.getClickedInventory().equals(event.getWhoClicked().getInventory())) {
+                    event.setCancelled(true);
+                }else if(event.getClick().isShiftClick()) {
+                    event.setCancelled(true);
+                }else if(event.getClick() == ClickType.DOUBLE_CLICK) {
+                    event.setCancelled(true);
                 }
             }
         }
     }
-
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onMove(final InventoryDragEvent e) {
-        if (getNoClick().contains(e.getWhoClicked().getUniqueId())) {
-            e.setCancelled(true);
+    public void onMove(final InventoryDragEvent event) {
+        if (getNoClick().contains(event.getWhoClicked().getUniqueId())) {
+            event.setCancelled(true);
         }
     }
-
     @EventHandler
-    public void onBuild(final PlayerInteractEvent e) {
-        Player p = e.getPlayer();
-        if(!PlotPlayer.from(p).getLocation().isPlotRoad()) {
-            if(PlotPlayer.from(p).getCurrentPlot() != null) {
-                if(!(PlotPlayer.from(p).getCurrentPlot().isOwner(p.getUniqueId()) || PlotPlayer.from(p).getCurrentPlot().isAdded(p.getUniqueId()))) {
-                    checkPerms(p, e);
+    public void onBuild(final PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if(!PlotPlayer.from(player).getLocation().isPlotRoad()) {
+            if(player.hasPermission(adminperms)) {
+                if(!(PlotPlayer.from(player).getCurrentPlot().isOwner(player.getUniqueId()) || PlotPlayer.from(player).getCurrentPlot().isAdded(player.getUniqueId()))) {
+                    checkPerms(player, event);
                 }
             }
-        }else checkPerms(p, e);
+        }else if(player.hasPermission(adminperms)) {
+            checkPerms(player, event);
+        }
     }
     @EventHandler
-    public void onEntity(PlayerInteractEntityEvent e) {
-        Player p = e.getPlayer();
-        if(!PlotPlayer.from(p).getLocation().isPlotRoad()) {
-            if(PlotPlayer.from(p).getCurrentPlot() != null) {
-                if(!(PlotPlayer.from(p).getCurrentPlot().isOwner(p.getUniqueId())  || PlotPlayer.from(p).getCurrentPlot().isAdded(p.getUniqueId()))) {
-                    checkOther(p, e);
+    public void onEntity(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        if(!PlotPlayer.from(player).getLocation().isPlotRoad()) {
+            if(player.hasPermission(adminperms)) {
+                if(!(PlotPlayer.from(player).getCurrentPlot().isOwner(player.getUniqueId())  || PlotPlayer.from(player).getCurrentPlot().isAdded(player.getUniqueId()))) {
+                    checkOther(player, event);
                 }
             }
-        }else checkOther(p, e);
+        }else if(player.hasPermission(adminperms)) {
+            checkOther(player, event);
+        }
     }
-
     @EventHandler (priority = EventPriority.HIGHEST)
-    public void onClose(final InventoryCloseEvent e) {
-        if (getNoClick().contains(e.getPlayer().getUniqueId())) {
-            getNoClick().remove(e.getPlayer().getUniqueId());
+    public void onClose(final InventoryCloseEvent event) {
+        if (getNoClick().contains(event.getPlayer().getUniqueId())) {
+            getNoClick().remove(event.getPlayer().getUniqueId());
         }
     }
-
-    public void allShulker(Player p, PlayerInteractEvent e) {
-        if (e.getClickedBlock().getType().equals(Material.SHULKER_BOX)) {
+    public void allShulker(Player p, PlayerInteractEvent event) {
+        if (event.getClickedBlock().getType().equals(Material.SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.yellow")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.YELLOW_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.YELLOW_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.yellow")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.BLACK_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.BLACK_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.black")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.BLUE_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.BLUE_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.blue")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.BROWN_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.BROWN_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.brown")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.CYAN_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.CYAN_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.cyan")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.GRAY_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.GRAY_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.gray")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.GREEN_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.GREEN_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.green")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.LIGHT_BLUE_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.LIGHT_BLUE_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.lightblue")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.LIGHT_GRAY_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.LIGHT_GRAY_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.lightgray")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.LIME_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.LIME_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.lime")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.MAGENTA_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.MAGENTA_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.magenta")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.ORANGE_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.ORANGE_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.orange")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.PINK_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.PINK_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.pink")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.PURPLE_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.PURPLE_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.purple")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.RED_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.RED_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.red")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getClickedBlock().getType().equals(Material.WHITE_SHULKER_BOX)) {
+        if (event.getClickedBlock().getType().equals(Material.WHITE_SHULKER_BOX)) {
             if (!p.hasPermission("system.plot.edit.shulkerbox.white")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
     }
-    private void checkPerms(Player p, PlayerInteractEvent e) {
-        if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            if (e.getClickedBlock().getType().equals(Material.CHEST)) {
+    private void checkPerms(Player p, PlayerInteractEvent event) {
+        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            if (event.getClickedBlock().getType().equals(Material.CHEST)) {
                 if (!p.hasPermission("system.plot.edit.chest")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.TRAPPED_CHEST)) {
+            if (event.getClickedBlock().getType().equals(Material.TRAPPED_CHEST)) {
                 if (!p.hasPermission("system.plot.edit.redstonechest")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.DROPPER)) {
+            if (event.getClickedBlock().getType().equals(Material.DROPPER)) {
                 if (!p.hasPermission("system.plot.edit.dropper")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.FURNACE)) {
+            if (event.getClickedBlock().getType().equals(Material.FURNACE)) {
                 if (!p.hasPermission("system.plot.edit.furnace")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.DISPENSER)) {
+            if (event.getClickedBlock().getType().equals(Material.DISPENSER)) {
                 if (!p.hasPermission("system.plot.edit.dispenser")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.HOPPER)) {
+            if (event.getClickedBlock().getType().equals(Material.HOPPER)) {
                 if (!p.hasPermission("system.plot.edit.hopper")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.BEACON)) {
+            if (event.getClickedBlock().getType().equals(Material.BEACON)) {
                 if (!p.hasPermission("system.plot.edit.beacon")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            if (e.getClickedBlock().getType().equals(Material.BREWING_STAND)) {
+            if (event.getClickedBlock().getType().equals(Material.BREWING_STAND)) {
                 if (!p.hasPermission("system.plot.edit.brewingstand")) {
-                    getNoClick().add(e.getPlayer().getUniqueId());
+                    getNoClick().add(event.getPlayer().getUniqueId());
                 }
             }
-            allShulker(p, e);
+            allShulker(p, event);
         }
     }
-    private void checkOther(Player p, PlayerInteractEntityEvent e) {
-        if (e.getRightClicked().getType().equals(EntityType.MINECART_HOPPER)) {
+    private void checkOther(Player p, PlayerInteractEntityEvent event) {
+        if (event.getRightClicked().getType().equals(EntityType.MINECART_HOPPER)) {
             if (!p.hasPermission("system.plot.edit.hopperminecart")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
-        if (e.getRightClicked().getType().equals(EntityType.MINECART_CHEST)) {
+        if (event.getRightClicked().getType().equals(EntityType.MINECART_CHEST)) {
             if (!p.hasPermission("system.plot.edit.chestminecart")) {
-                getNoClick().add(e.getPlayer().getUniqueId());
+                getNoClick().add(event.getPlayer().getUniqueId());
             }
         }
     }
